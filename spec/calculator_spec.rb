c18bc584fe2c6112a466ffb4b5174d36be7f28a5
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-10-09 01:23:34 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,8 +3,13 @@
 describe "./calculator.rb" do
 
 
+<<<<<<< HEAD
   it " contains a local variable called first_number that is assigned to a number" do
 
+=======
+  it "contains a local variable called first_number that is assigned to a number" do
+    first_number = 10.0
+>>>>>>> 8851368dfdd37fe11d76dcf12f87937a3c8d9ed7
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
     expect(first_number).to be_an(Integer).or be_a(Float)
